f872a1f00c153c0f3e982bb1d20200218120832a
mboroki-elvis <69868982+mboroki-elvis@users.noreply.github.com>
2023-12-19 11:22:20 +0300

    add some tests

--- git hub users/GitHubUsersTests/GitHubUsersTests.swift
+++ git hub users/GitHubUsersTests/GitHubUsersTests.swift
@@ -1,29 +1,38 @@
+@testable import GitHubUsers
 import XCTest
-@testable import git_hub_users
 
 final class GitHubUsersTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    var sut: AuthenticationViewModel!
+    var service: AuthenticationService!
+    var environment: AppEnvironment!
+    override func setUp() {
+        super.setUp()
+        environment = EnvironmentMock()
+        service = AuthenticationServiceImplementation(environment: environment)
+        sut = .init(service: service)
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    override func tearDown() {
+        service = nil
+        sut = nil
+        super.tearDown()
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    func testStartFlow() throws {
+        let router = AppRouter(.auth(.authorize))
+        sut.startFlow(router: router)
+        XCTAssert(sut.error == nil)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testStartFlowGetToken() throws {
+        let router = AppRouter(.auth(.authorize))
+        sut.startFlow(router: router)
+        XCTAssert(sut.error == nil)
+        let state = environment.keychainManager.string(forKey: .authState) ?? ""
+        let url = "com.ketu.gitusers://authentication?code=24828ba&state=\(state)"
+        sut.obtainAccessToken(url: URL(string: url)!, router: router)
+        XCTAssert(sut.error == nil)
     }
-
 }
+
+
